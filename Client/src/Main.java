afb9fd93e09bdf5378fdabd166907ca0fa9160cd
Ridleh <42791844+Ridleh@users.noreply.github.com>
2020-02-01 12:39:42 -0500

    Revert "Update Main.java"
    This reverts commit e220ab21c3ca5a2b6ced768f189a42220851b0ae.

--- Client/src/Main.java
+++ Client/src/Main.java
@@ -17,6 +17,5 @@ public class Main extends Application {
     @Override   
     public void start(Stage stage) {
         GraphicalUserInterface.getInstance().start(stage);
-        //boop
     }
 }
