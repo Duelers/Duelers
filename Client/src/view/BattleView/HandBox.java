b7e0247aa9fbf83ce3942a08ad569ebb698c7763
Ridleh <42791844+Ridleh@users.noreply.github.com>
2020-02-02 23:32:03 -0500

    Update HandBox.java
    Updated Handbox.java based on feedback

--- Client/src/view/BattleView/HandBox.java
+++ Client/src/view/BattleView/HandBox.java
@@ -87,8 +87,6 @@ public class HandBox implements PropertyChangeListener {
         addFinishButton();
     }
 
-    //Creates the blue circle on the bottom left side of the screen.
-    //Also displays the next card the player will draw at the end of their turn.
     private void updateNext() {
         next.getChildren().clear();
         final ImageView imageView1 = new ImageView();
@@ -102,36 +100,19 @@ public class HandBox implements PropertyChangeListener {
         imageView2.setFitWidth(Constants.SCREEN_WIDTH * 0.11);
         imageView2.setFitHeight(Constants.SCREEN_WIDTH * 0.11);
 
-
-        /*
-        The code below is what displays the next card the player will draw on top of the blue circle
-
-        final CardAnimation cardAnimation;
-        if (player.getNextCard() != null) {
-            cardAnimation = new CardAnimation(next, player.getNextCard(),
-                    imageView1.getLayoutY() + imageView1.getFitHeight() / 2, imageView1.getLayoutX() + imageView1.getFitWidth() / 2);
-        } else {
-            cardAnimation = null;
-        }
-
-         */
-
         CardAnimation cardAnimation = null;
 
         imageView2.setImage(nextRingSmoke);
 
-        if (cardAnimation != null ) {
-            next.setOnMouseEntered(mouseEvent -> {
-                if (battleScene.isMyTurn()) {
-                    //cardAnimation.inActive();
-                    imageView2.setImage(nextRingShine);
-                }
-            });
-            next.setOnMouseExited(mouseEvent -> {
-                //cardAnimation.pause();
-                imageView2.setImage(nextRingSmoke);
-            });
-        }
+        next.setOnMouseEntered(mouseEvent -> {
+            if (battleScene.isMyTurn()) {
+                imageView2.setImage(nextRingShine);
+            }
+        });
+
+        next.setOnMouseExited(mouseEvent -> {
+            imageView2.setImage(nextRingSmoke);
+        });
     }
 
     private void updateCards() {
@@ -150,6 +131,9 @@ public class HandBox implements PropertyChangeListener {
             } else {
                 cardAnimation = null;
             }
+
+
+
 
             if (selectedCard == i && cardAnimation != null) {
                 imageView.setImage(cardBackGlow);
