113d9c3621b85a6e7b1ea84c80ddd11fc6fac52e
Chris <elysuim_1@hotmail.com>
2020-02-05 04:47:26 +0000

    Update cleanup.py

--- Build_exe/cleanup.py
+++ Build_exe/cleanup.py
@@ -3,18 +3,25 @@
 
     Run this to return the Application directory back to is defualt state.
 
-    This script is useful to run in a build failed previously since it will (attempt to) clean up any mess
+    This script is useful to run in a build failed previously since it will (attempt to) clean up any mess.
+
+    For best results run as admin.
 """
 
 import os 
 import shutil
-
+import stat 
 SCRIPT_DIR = os.path.dirname(__file__)
+
+def redo_with_write(redo_func, path, err):
+    os.chmod(path, stat.S_IWRITE)
+    redo_func(path)
+
 
 if __name__ == "__main__":
     print("deleting application folder...")
     app = os.path.join(SCRIPT_DIR, "application")
-    shutil.rmtree(app)
+    shutil.rmtree(app, onerror=redo_with_write)
 
     print("recreating empty folder structure")
     os.makedirs(os.path.join(SCRIPT_DIR, "application", "Client"))
