80f4a10562437ff9b1b302f85311dc48dd2df641
Chris <elysuim_1@hotmail.com>
2020-02-05 20:30:33 +0000

    Update cleanup.py

--- Build_exe/cleanup.py
+++ Build_exe/cleanup.py
@@ -1,21 +1,25 @@
-"""
-    Cleanup
-
-    Run this to return the Application directory back to is defualt state.
-
-    This script is useful to run in a build failed previously since it will (attempt to) clean up any mess.
-
-    For best results run as admin.
-"""
-
 import os 
 import shutil
 import stat 
+
+"""
+    Cleanup
+
+    Run this to return the build_exe/Application directory back to is default state.
+    This script is useful to run if a build failed previously and you wish to clean up any mess.
+
+    For best results RUN AS ADMINISTRATOR.
+"""
+
 SCRIPT_DIR = os.path.dirname(__file__)
 
-def redo_with_write(redo_func, path, err):
+def redo_with_write(func, path, err):
+    """
+    Change permissions of file/directory.
+    We then call func(path) 
+    """
     os.chmod(path, stat.S_IWRITE)
-    redo_func(path)
+    func(path)
 
 
 if __name__ == "__main__":
@@ -23,6 +27,9 @@ if __name__ == "__main__":
     app = os.path.join(SCRIPT_DIR, "application")
     shutil.rmtree(app, onerror=redo_with_write)
 
-    print("recreating empty folder structure")
+    # Default Structure is empty 'jre' and 'Client' folders inside build_exe/Application
+    print("recreating empty folder structure...")
     os.makedirs(os.path.join(SCRIPT_DIR, "application", "Client"))
     os.makedirs(os.path.join(SCRIPT_DIR, "application", "jre"))
+
+    print("Clean up Script Complete.")
