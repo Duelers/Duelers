89dfdc572fe1671bfa87eeb4cd93efb6e8e957d1
Chris <elysuim_1@hotmail.com>
2020-02-07 18:39:15 +0000

    Added Alt+Enter Keyboard Shortcut

---
+++ Client/src/models/gui/KeyboardShortcutConstants.java
@@ -0,0 +1,10 @@
+package models.gui;
+
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+
+public class KeyboardShortcutConstants {
+    public static final KeyCombination EXIT_FULLSCREEN = new KeyCodeCombination(KeyCode.ENTER, KeyCombination.ALT_DOWN);
+    public static final String EXIT_FULLSCREEN_HELP_MSG = "Exit fullscreen: Alt+Enter";
+}
