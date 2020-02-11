9be0995b93a561ddfcd18b39095b432fb3112776
Chris <elysuim_1@hotmail.com>
2020-02-11 02:11:31 +0000

    add script

---
+++ devTools/cardCreator/AddJsonCardToGame.py
@@ -0,0 +1,128 @@
+import os
+import json
+import shutil
+import copy
+
+from typing import Dict, Any, List
+
+SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
+
+RESOURCES_DIR = os.path.abspath(os.path.join(SCRIPT_PATH, "..", "..", "resources"))
+RESOURCES_SERVER_DIR = os.path.abspath(os.path.join(SCRIPT_PATH, "..", "..", "Server", "resources"))
+
+ACCOUNTS_DIR = os.path.join(RESOURCES_SERVER_DIR, "accounts")
+
+RESOURCES_SUBFOLDER_NAMES = ["heroCards", "minionCards", "spellCards"]
+SUPPORTED_CARD_TYPES = ["HERO", "MINION", "SPELL"]
+
+subfolder_for_type = dict(zip(SUPPORTED_CARD_TYPES, RESOURCES_SUBFOLDER_NAMES)) 
+
+def load_json(fullpath: str) -> Dict[Any, Any]:
+    with open(fullpath) as f:
+        data = json.load(f)
+    return data
+
+def is_name_valid(filename: str, card_type: str, cardId: str) -> bool:
+    check_1 = filename.endswith(f".{card_type.lower()}.card.json")
+    check_2 = filename.startswith(cardId)
+    
+    return check_1 and check_2
+
+def get_card_id_for_account(account_name: str, card_id: str, i: int) -> str:
+    acc_name = account_name.replace(" ", "")
+    return f"{acc_name}_{card_id}_{i}"
+
+def make_copies(ids: List[str], data: Dict[Any, Any]) -> List[Dict[Any,Any]]:
+
+    c1 = data.copy() 
+    c1["cardId"] = acc_ids[0]
+
+    if len(ids) == 1:
+        return [c1]
+    
+    else:
+        c2 = data.copy()
+        c3 = data.copy() 
+
+        c2["cardId"] = acc_ids[1]
+        c3["cardId"] = acc_ids[2]
+
+        return [c1, c2, c3]
+
+
+if __name__ == "__main__":
+
+    print("+===========================================+")
+    print("|    NEW CARD INSTALLER SCRIPT ver1.0       |")
+    print("+===========================================+")
+
+    assert os.path.isdir(RESOURCES_DIR), "ERROR: {RESOURCES_PATH} is not a valid directory!"
+    assert os.path.isdir(RESOURCES_SERVER_DIR), "ERROR: {RESOURCES_SERVER_PATH} is not a valid directory!"
+    assert os.path.isdir(ACCOUNTS_DIR), "ERROR: {ACCOUNT_PATH} is not a valid directory!"
+
+    card: str = ""
+    for f in os.listdir(SCRIPT_PATH):
+        if f.endswith(".card.json"):
+            card = f
+            break
+    assert card, f"ERROR: no card was found in {SCRIPT_PATH}"
+    
+    print(f"Card Found: {card}")
+    card_path = os.path.join(SCRIPT_PATH, card)
+
+    print("loading json...")
+    data = load_json(card_path)
+
+    card_type = data["type"]
+    assert card_type in SUPPORTED_CARD_TYPES, f"ERROR: '{card_type}' is not a supported card type."
+
+    card_id   = data["cardId"]
+    assert (" " not in card_id), "ERROR: Spaces should not be in cardId field"
+
+    assert is_name_valid(card, card_type, card_id), f"ERROR: '{card}' does not follow naming convention."
+
+    loc_1 = os.path.join(RESOURCES_DIR, subfolder_for_type[card_type])
+    print(f"attempting to copy card to dir: {loc_1}")
+    shutil.copy(card_path, loc_1)
+
+    loc_2 = os.path.join(RESOURCES_SERVER_DIR, subfolder_for_type[card_type])
+    print(f"attempting to copy card to dir: {loc_2}")
+    shutil.copy(card_path, loc_2)
+
+    ACCOUNT_NAME = "john_doe"
+    account_path = os.path.join(ACCOUNTS_DIR, ACCOUNT_NAME + ".account.json")
+    assert os.path.exists(account_path), f"ERROR: '{account_path}' is not a file!"
+
+    print(f"Attempting to add card to account: {ACCOUNT_NAME}")
+    acc_json = load_json(account_path)
+    username = acc_json["username"]
+    
+    if card_type != "HERO":
+        print("Adding 3x copies...")
+        ## Creates the new account id's for all three copies. Each new id follows the format: '{username}_{cardid}_{number}'
+        acc_ids = [get_card_id_for_account(username, card_id, i) for i in range(1, 4)]
+    else:
+        print("Adding 1x copy...")
+        acc_ids = [get_card_id_for_account(username, card_id, 1)]
+
+    card_objects = make_copies(acc_ids, data)
+
+    collection = acc_json["collection"][card_type.lower() + "s"]
+    for idx, collected_card in enumerate(collection): 
+        if collected_card["cardId"] in acc_ids:
+            print(f"Card was found in account, overwriting")
+
+            x_card =  acc_ids.index(collected_card["cardId"])
+            collection[idx] = card_objects[x_card]
+            
+            acc_ids.pop(x_card)
+            card_objects.pop(x_card) ## pop both to keep index in sync
+
+    for copy in card_objects:
+        collection.append(copy)
+
+    print("Saving changes made to account")
+    with open(account_path, "w") as jsonFile:
+        json.dump(acc_json, jsonFile, indent=4)
+
+    print("SCRIPT COMPLETE!")
