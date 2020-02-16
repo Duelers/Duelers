968d3e3b0fd6e08d14d3a32ce9a837c75793a527
Chris <elysuim_1@hotmail.com>
2020-02-16 06:17:41 +0000

    Update AbabeelLogoBox.java
    change login tooltip

--- Client/src/models/gui/AbabeelLogoBox.java
+++ Client/src/models/gui/AbabeelLogoBox.java
@@ -42,7 +42,7 @@ class AbabeelLogoBox extends VBox {
 
     private Text makeAbabeelText() {
         return new DefaultText(
-                "For all cards and default decks:\nLogin: 'john doe'\nPASSWORD: 'qwer'",
+                "For Duelyst cards and decks:\nLogin: 'duelyst'\nPASSWORD: 'duel'",
                 BOX_SIZE - UIConstants.DEFAULT_SPACING * 5,
                 FONT, ABABEEL_TEXT_COLOR
         );
