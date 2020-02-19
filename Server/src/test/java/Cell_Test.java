c13b5bc5f1cf421e701dad17a29b85bc94d41f53
Chris <elysuim_1@hotmail.com>
2020-02-19 00:37:34 +0000

    Update Cell_Test.java

--- Server/src/test/java/Cell_Test.java
+++ Server/src/test/java/Cell_Test.java
@@ -8,9 +8,18 @@ import server.gameCenter.models.map.Cell;
 public class Cell_Test {
 
     @Test
-    public void manhattenDistance_ReturnsTwo_WhenSquareIsTwoAway(){
+    public void manhattenDistance_ReturnsTwo_WhenCellIsTwoAway(){
         Cell cell1 = new Cell(0, 0);
         Cell cell2 = new Cell(0, 2);
+
+        Assert.assertEquals(2, cell1.manhattanDistance(cell2));
+        Assert.assertEquals(2, cell2.manhattanDistance(cell1));
+    }
+
+    @Test
+    public void manhattenDistance_ReturnsTwo_WhenCellIsOneAwayDiagonally(){
+        Cell cell1 = new Cell(0, 0);
+        Cell cell2 = new Cell(1, 1);
 
         Assert.assertEquals(2, cell1.manhattanDistance(cell2));
         Assert.assertEquals(2, cell2.manhattanDistance(cell1));
