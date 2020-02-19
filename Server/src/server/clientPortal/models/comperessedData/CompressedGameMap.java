74e03fffca3770bb6745833887f6dce49358783f
Chris <elysuim_1@hotmail.com>
2020-02-19 23:52:40 +0000

    Delete compressed cell (#57)
    * Update Cell_Test.java
    
    * Update Cell.java
    
    * Update Cell.java
    
    * documentation
    
    * del
    
    Co-authored-by: Ridleh <42791844+Ridleh@users.noreply.github.com>

--- Server/src/server/clientPortal/models/comperessedData/CompressedGameMap.java
+++ Server/src/server/clientPortal/models/comperessedData/CompressedGameMap.java
@@ -9,13 +9,13 @@ import java.util.List;
 public class CompressedGameMap {
     private static final int ROW_NUMBER = 5, COLUMN_NUMBER = 9;
 
-    private CompressedCell[][] cells = new CompressedCell[ROW_NUMBER][COLUMN_NUMBER];
+    private Cell[][] cells = new Cell[ROW_NUMBER][COLUMN_NUMBER];
     private List<CompressedTroop> troops = new ArrayList<>();
 
     public CompressedGameMap(Cell[][] cells, List<Troop> troops) {
         for (int i = 0; i < ROW_NUMBER; i++) {
             for (int j = 0; j < COLUMN_NUMBER; j++) {
-                this.cells[i][j] = cells[i][j].toCompressedCell();
+                this.cells[i][j] = cells[i][j];
             }
         }
         for (Troop troop : troops) {
