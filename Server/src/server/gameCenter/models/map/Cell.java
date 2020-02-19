33ee6b15fcd2d99cd6774245427155d6f6b7726c
Chris <elysuim_1@hotmail.com>
2020-02-19 14:09:21 +0000

    Update Cell.java

--- Server/src/server/gameCenter/models/map/Cell.java
+++ Server/src/server/gameCenter/models/map/Cell.java
@@ -1,6 +1,7 @@
 package server.gameCenter.models.map;
 
 import server.clientPortal.models.comperessedData.CompressedCell;
+import java.util.ArrayList;
 
 public class Cell {
     private int row;
@@ -37,6 +38,31 @@ public class Cell {
 
     public boolean isNextTo(Cell cell) {
         return Math.abs(cell.row - row) < 2 && Math.abs(cell.column - column) < 2;
+    }
+
+
+    public ArrayList<Cell> getNeighbourCells(int numRows, int numCols) {
+
+        ArrayList<Cell> cells = new ArrayList<>();
+
+        short[] offsets = {-1, 0, 1};
+        for (short i : offsets) {
+            for (short j : offsets) {
+
+                int r2 = i + row;
+                int c2 = j + column;
+
+                // Check cells bounds, we want all index to be positive and lew than the upper bound.
+                boolean checkRow = (r2 >= 0) ? r2 < numRows : false;
+                boolean checkCol = (c2 >= 0) ? c2 < numCols : false;
+                boolean checkIdentity = (r2 == row) ? c2 == column : false; // don't add the current cell itself
+
+                if (checkRow && checkCol && !checkIdentity) {
+                    cells.add(new Cell(row + i, column + j));
+                }
+            }
+        }
+        return cells;
     }
 
     public int manhattanDistance(Cell otherCell) {
