04478ea1978e359f88dc27f5fccd102bf4629b45
Chris <elysuim_1@hotmail.com>
2020-02-26 09:47:47 +0000

    moved keycode.T

--- Client/src/models/Constants.java
+++ Client/src/models/Constants.java
@@ -6,5 +6,4 @@ public class Constants {
     public static final int PORT = 8888;
     public static final String SERVER_IP = "localhost";
     public static final String SERVER_NAME = "Server";
-    public static final KeyCode KEY_FOR_CHAT = KeyCode.T;
 }
