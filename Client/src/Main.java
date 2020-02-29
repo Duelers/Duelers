ca8090c30af909cf5c15595ec6926b821ee9876f
Chris <elysuim_1@hotmail.com>
2020-02-29 19:32:01 +0000

    Update Main.java

--- Client/src/Main.java
+++ Client/src/Main.java
@@ -14,22 +14,7 @@ import java.lang.reflect.InvocationTargetException;
 
 public class Main extends Application {
 
-    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, NoSuchFieldException {
-
-        LoadLanguage x = new LoadLanguage("english");
-        x.getValue(new String[] {"LOGIN_MENU", "LOGIN"});
-
-        //String restaurantJson = "{'LANGUAGE_ENGLISH_TITLE':'english','LANGUAGE_NATIVE_TITLE':'english','LOGIN_MENU':{'WELCOME_MESSAGE':'The Community tries to save Duelyst.','VERSION':'version','LOGIN':'login','REGISTER':'register','PASSWORD':'password','ERROR_SHORT_PASSWORD':'Error! Password is too short.','ERROR_SHORT_USERNAME':'Error! Username is too short.','ERROR_INCORRECT_PASSWORD':'Error! Incorrect Password.'},'BUTTON_TEXT':{'OK':'ok','CANCEL':'cancel'}}";
-
-        //String restaurantJson = new FileReader( "resources/configurations/Languages/english.json");
-
-        Gson gson = new Gson();
-
-        Language restaurantObject = gson.fromJson(new FileReader( "resources/configurations/Languages/english.json"), Language.class);
-
-        String o = restaurantObject.LANGUAGE_ENGLISH_TITLE;
-
-        String y = restaurantObject.BUTTON_TEXT.OK;
+    public static void main(String[] args){
 
 
         String hostServer = Config.getInstance().getProperty("HOST_SERVER");
