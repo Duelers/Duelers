6e9bcd703c9d1cd855700f8245c4fb5a88ae4b61
Chris <elysuim_1@hotmail.com>
2020-03-03 18:31:41 +0000

    Update Language.java

--- Client/src/models/languageLocalisation/Language.java
+++ Client/src/models/languageLocalisation/Language.java
@@ -6,5 +6,8 @@ public class Language {
     public String LANGUAGE_NATIVE_TITLE;
 
     public loginMenu LOGIN_MENU;
+    public mainMenu MAIN_MENU;
+    public spectate SPECTATE;
+    public profile PROFILE;
     public buttonText BUTTON_TEXT;
 }
