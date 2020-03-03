6a1f1e37e02145ee97712af46553de204cc786e9
Chris <elysuim_1@hotmail.com>
2020-03-03 18:28:16 +0000

    Update buttonText.java

--- Client/src/models/languageLocalisation/buttonText.java
+++ Client/src/models/languageLocalisation/buttonText.java
@@ -2,6 +2,7 @@ package models.languageLocalisation;
 
 public class buttonText {
         public String OK;
+        public String START;
         public String CANCEL;
         public String ERROR;
 }
