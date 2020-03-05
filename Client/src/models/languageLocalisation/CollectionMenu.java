0cf76bde602570494669e62963e32b310653eaa4
Chris <elysuim_1@hotmail.com>
2020-03-05 23:58:59 +0000

    Create CollectionMenu.java

---
+++ Client/src/models/languageLocalisation/CollectionMenu.java
@@ -0,0 +1,12 @@
+package models.languageLocalisation;
+
+public class CollectionMenu {
+    public String NEW_DECK;
+    public String IMPORT_DECK;
+    public String CARDS;
+    public String HERO;
+    public String DECK_NAME_PROMPT;
+    public String DECK_NAME;
+    public String DECK_CARD_ADD_ERROR;
+    public String DECK_CARD_REMOVE_ERROR;
+}
