0f5e9ed1c651f24ba0d55a06b6b3705d087a2fc1
Oleg <maslennikovoleg@gmail.com>
2020-03-05 23:02:52 +0300

    Fix NPE

--- Shared/src/shared/models/services/token/AuthenticationTokenResponse.java
+++ Shared/src/shared/models/services/token/AuthenticationTokenResponse.java
@@ -4,7 +4,7 @@ import shared.models.services.WebApiResponse;
 
 public class AuthenticationTokenResponse implements WebApiResponse {
     
-    public String error;
+    public String error = null;
     public String username;
 
     public AuthenticationTokenResponse() {
