423cd6e39c3da2a67d956166623e728d0f80b2d6
Toren Darby <darby@science.ubc.ca>
2020-03-12 13:50:44 -0700

    Merge Move to shared

---
+++ Shared/src/shared/models/game/availableactions/BaseMove.java
@@ -0,0 +1,25 @@
+package shared.models.game.availableactions;
+
+import shared.models.game.Troop;
+import shared.models.game.map.Cell;
+
+import java.util.Collections;
+import java.util.List;
+
+public class BaseMove<TroopType extends Troop> {
+    private final TroopType troop;
+    private final List<Cell> targets;
+
+    public BaseMove(TroopType troop, List<Cell> targets) {
+        this.troop = troop;
+        this.targets = targets;
+    }
+
+    public TroopType getTroop() {
+        return troop;
+    }
+
+    public List<Cell> getTargets() {
+        return Collections.unmodifiableList(targets);
+    }
+}
