63ca648feb31f1b61f5dbe679729b5563838b31b
Chris <elysuim_1@hotmail.com>
2020-03-14 22:16:39 +0000

    constant (#405)

--- Shared/src/shared/Constants.java
+++ Shared/src/shared/Constants.java
@@ -1,6 +1,9 @@
 package shared;
-
 public class Constants {
+    // Board
     public static final int NUMBER_OF_ROWS = 5;
     public static final int NUMBER_OF_COLUMNS = 9;
+
+    // Turn
+    public static final long TURN_TIME_LIMIT = 120; // secs
 }
