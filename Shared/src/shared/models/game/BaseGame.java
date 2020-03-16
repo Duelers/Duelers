a67c32d1032289899dd44d6fe64bdb61468e15bc
Toren Darby <darby@science.ubc.ca>
2020-03-16 00:44:54 -0700

    Merge client CompressedGame into BaseGame

--- Shared/src/shared/models/game/BaseGame.java
+++ Shared/src/shared/models/game/BaseGame.java
@@ -3,11 +3,11 @@ package shared.models.game;
 import shared.models.game.map.BaseGameMap;
 
 public class BaseGame<PlayerType extends BasePlayer, GameMapType extends BaseGameMap> {
-    private final PlayerType playerOne;
-    private final PlayerType playerTwo;
-    private final GameMapType gameMap;
-    private final int turnNumber;
-    private final GameType gameType;
+    protected final PlayerType playerOne;
+    protected final PlayerType playerTwo;
+    protected final GameMapType gameMap;
+    protected int turnNumber;
+    protected final GameType gameType;
 
     public BaseGame(PlayerType playerOne, PlayerType playerTwo, GameMapType gameMap, int turnNumber, GameType gameType) {
         this.playerOne = playerOne;
@@ -15,6 +15,42 @@ public class BaseGame<PlayerType extends BasePlayer, GameMapType extends BaseGam
         this.gameMap = gameMap;
         this.turnNumber = turnNumber;
         this.gameType = gameType;
+    }
+
+    public PlayerType getPlayerOne() {
+        return playerOne;
+    }
+
+    public PlayerType getPlayerTwo() {
+        return playerTwo;
+    }
+
+    public GameMapType getGameMap() {
+        return gameMap;
+    }
+
+    public int getTurnNumber() {
+        return turnNumber;
+    }
+
+    public GameType getGameType() {
+        return gameType;
+    }
+
+    public PlayerType getCurrentTurnPlayer() {
+        return getPlayer(turnNumber % 2);
+    }
+
+    public PlayerType getOtherTurnPlayer() {
+        return getPlayer(turnNumber % 2 + 1);
+    }
+
+    protected PlayerType getPlayer(int number) {
+        if (number == 1) {
+            return playerOne;
+        } else {
+            return playerTwo;
+        }
     }
 
 }
