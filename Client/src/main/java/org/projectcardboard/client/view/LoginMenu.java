5eb9292ba424022d8df65ae9bd2626d9a77ab3e4
Chris <elysuim_1@hotmail.com>
2020-03-17 21:43:35 +0000

    Update LoginMenu.java

--- Client/src/main/java/org/projectcardboard/client/view/LoginMenu.java
+++ Client/src/main/java/org/projectcardboard/client/view/LoginMenu.java
@@ -1,5 +1,6 @@
 package org.projectcardboard.client.view;
 
+import Config.Config;
 import org.projectcardboard.client.controller.GraphicalUserInterface;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
@@ -13,6 +14,8 @@ import org.projectcardboard.client.models.localisation.LanguageKeys;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.projectcardboard.client.models.gui.UIConstants.SCALE;
 
@@ -29,17 +32,46 @@ public class LoginMenu extends Show {
 
             root.getChildren().addAll(sceneContents);
 
-
             String versionInfo = getClientVersionInfo();
             if (versionInfo != null) {
                 String version = LanguageData.getInstance().getValue(new String[] {LanguageKeys.LOGIN_MENU, LanguageKeys.VERSION});
-                DefaultLabel versionLabel = new DefaultLabel(String.format("%s: %s", version, versionInfo), Font.font("SansSerif", FontWeight.EXTRA_BOLD, 40 * SCALE), Color.WHITE);
+                DefaultLabel versionLabel = new DefaultLabel(String.format("%s: %s", version, versionInfo), Font.font("SansSerif", FontWeight.EXTRA_BOLD, 40 * SCALE), Color.DARKGRAY, 10, 10);
                 root.getChildren().add(versionLabel);
             }
+
+
+            // Todo add Localization of 'server',  'local host'
+            String uri = Config.getInstance().getProperty("SERVER_URI");
+            Boolean isLocalConnection = Boolean.parseBoolean(Config.getInstance().getProperty("HOST_SERVER"));
+            String ServerName = String.format("server: %s", isLocalConnection ? "local host" : uri);
+            DefaultLabel ServerLabel = new DefaultLabel(ServerName, Font.font("SansSerif", FontWeight.EXTRA_BOLD, 40 * SCALE), Color.DARKGRAY, 10, 30);
+            root.getChildren().add(ServerLabel);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public String getServerInfo(){
+        Boolean isLocalConnection = Boolean.parseBoolean(Config.getInstance().getProperty("HOST_SERVER"));
+        if (isLocalConnection){
+            return "local host";
+        }
+
+        String uri = Config.getInstance().getProperty("SERVER_URI");
+        Pattern p = Pattern.compile("/(\\w*$)");
+        Matcher m = p.matcher(uri);
+        m.matches();
+
+        String serverName = "Production";
+        try {
+            serverName = m.group(0);
+        }
+        catch(Exception e){
+            // do nothing
+        }
+
+        return serverName;
     }
 
     public String getClientVersionInfo() throws IOException {
