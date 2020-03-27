f2147cd6f7b64828b1f0d8e3daf50fd2e8421073
Chris <elysuim_1@hotmail.com>
2020-03-27 19:55:28 +0000

    style

--- Client/src/main/java/org/projectcardboard/client/models/card/Deck.java
+++ Client/src/main/java/org/projectcardboard/client/models/card/Deck.java
@@ -9,77 +9,77 @@ import org.projectcardboard.client.models.account.Collection;
 
 public class Deck extends BaseDeck<Card> {
 
-    public Deck(TempDeck tempDeck, Collection collection) {
-        super(tempDeck.getDeckName());
-        this.hero = collection.findHero(tempDeck.getHeroId());
-        for (String cardId : tempDeck.getCardIds()) {
-            this.cards.add(collection.findOthers(cardId));
-        }
+  public Deck(TempDeck tempDeck, Collection collection) {
+    super(tempDeck.getDeckName());
+    this.hero = collection.findHero(tempDeck.getHeroId());
+    for (String cardId : tempDeck.getCardIds()) {
+      this.cards.add(collection.findOthers(cardId));
     }
+  }
 
-    // TODO: overriding equals without changing hashcode is normally no bueno.
-    // there's probably a safer way for us to accomplish this.
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null)
-            return false;
-        if (obj instanceof Deck) {
-            Deck deck = (Deck) obj;
-            if (!deckName.equals(deck.getName()))
-                return false;
-            if (hero == null ^ deck.hero == null)
-                return false;
-            if (hero != null && !hero.equals(deck.hero))
-                return false;
-
-            if (cards.size() != deck.cards.size())
-                return false;
-            for (Card cardInThis : cards) {
-                if (!deck.cards.contains(cardInThis))
-                    return false;
-            }
-            return true;
-        }
-        if (obj instanceof TempDeck) {
-            TempDeck deck = (TempDeck) obj;
-            if (!deckName.equals(deck.getDeckName()))
-                return false;
-            if (hero == null ^ deck.getHeroId() == null)
-                return false;
-            if (hero != null && !hero.getCardId().equalsIgnoreCase(deck.getHeroId()))
-                return false;
-
-            if (cards.size() != deck.getCardIds().size())
-                return false;
-            for (Card cardInThis : cards) {
-                if (!deck.getCardIds().contains(cardInThis.getCardId()))
-                    return false;
-            }
-            return true;
-        }
+  // TODO: overriding equals without changing hashcode is normally no bueno.
+  // there's probably a safer way for us to accomplish this.
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == null)
+      return false;
+    if (obj instanceof Deck) {
+      Deck deck = (Deck) obj;
+      if (!deckName.equals(deck.getName()))
         return false;
-    }
+      if (hero == null ^ deck.hero == null)
+        return false;
+      if (hero != null && !hero.equals(deck.hero))
+        return false;
 
-    public boolean isMain() {
-        return Client.getInstance().getAccount().isMainDeck(this);
+      if (cards.size() != deck.cards.size())
+        return false;
+      for (Card cardInThis : cards) {
+        if (!deck.cards.contains(cardInThis))
+          return false;
+      }
+      return true;
     }
+    if (obj instanceof TempDeck) {
+      TempDeck deck = (TempDeck) obj;
+      if (!deckName.equals(deck.getDeckName()))
+        return false;
+      if (hero == null ^ deck.getHeroId() == null)
+        return false;
+      if (hero != null && !hero.getCardId().equalsIgnoreCase(deck.getHeroId()))
+        return false;
 
-    public int count(ICard card) {
-        switch (card.getType()) {
-            case HERO:
-                if (hero != null && hero.isSameAs(card.getName()))
-                    return 1;
-                return 0;
-            case MINION:
-            case SPELL:
-                int count = 0;
-                for (Card cardInThis : cards) {
-                    if (cardInThis.isSameAs(card.getName()))
-                        count++;
-                }
-                return count;
-            default:
-                return 0;
+      if (cards.size() != deck.getCardIds().size())
+        return false;
+      for (Card cardInThis : cards) {
+        if (!deck.getCardIds().contains(cardInThis.getCardId()))
+          return false;
+      }
+      return true;
+    }
+    return false;
+  }
+
+  public boolean isMain() {
+    return Client.getInstance().getAccount().isMainDeck(this);
+  }
+
+  public int count(ICard card) {
+    switch (card.getType()) {
+      case HERO:
+        if (hero != null && hero.isSameAs(card.getName()))
+          return 1;
+        return 0;
+      case MINION:
+      case SPELL:
+        int count = 0;
+        for (Card cardInThis : cards) {
+          if (cardInThis.isSameAs(card.getName()))
+            count++;
         }
+        return count;
+      default:
+        return 0;
     }
+  }
 }
--- Client/src/test/java/org/projectcardboard/client/models/gui/DeckBox_Test.java
+++ Client/src/test/java/org/projectcardboard/client/models/gui/DeckBox_Test.java
@@ -26,51 +26,51 @@ import static org.mockito.Mockito.when;
 
 public class DeckBox_Test extends ApplicationTest {
 
-    private DeckBox testDeckBox;
-    private Stage stage;
+  private DeckBox testDeckBox;
+  private Stage stage;
 
-    @Override
-    public void start(Stage stage) {
-        this.stage = stage;
-    }
+  @Override
+  public void start(Stage stage) {
+    this.stage = stage;
+  }
 
-    @Mock
-    Deck mockDeck;
+  @Mock
+  Deck mockDeck;
 
-    @BeforeClass
-    public static void setupSpec() throws Exception {
-        System.setProperty("testfx.robot", "glass");
-        System.setProperty("testfx.headless", "true");
-        System.setProperty("prism.order", "sw");
-        System.setProperty("prism.text", "t2k");
-        System.setProperty("java.awt.headless", "true");
-    }
+  @BeforeClass
+  public static void setupSpec() throws Exception {
+    System.setProperty("testfx.robot", "glass");
+    System.setProperty("testfx.headless", "true");
+    System.setProperty("prism.order", "sw");
+    System.setProperty("prism.text", "t2k");
+    System.setProperty("java.awt.headless", "true");
+  }
 
-    @Before
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
-        when(mockDeck.getName()).thenReturn("mockDeckName");
+  @Before
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+    when(mockDeck.getName()).thenReturn("mockDeckName");
 
-        Platform.runLater(() -> {
-            testDeckBox = new DeckBox(mockDeck);
-            stage.setScene(new Scene(new StackPane(testDeckBox), 100, 100));
-            stage.show();
-        });
-        WaitForAsyncUtils.waitForFxEvents();
-    }
+    Platform.runLater(() -> {
+      testDeckBox = new DeckBox(mockDeck);
+      stage.setScene(new Scene(new StackPane(testDeckBox), 100, 100));
+      stage.show();
+    });
+    WaitForAsyncUtils.waitForFxEvents();
+  }
 
-    @After
-    public void tearDown() throws Exception {
-        FxToolkit.hideStage();
-    }
+  @After
+  public void tearDown() throws Exception {
+    FxToolkit.hideStage();
+  }
 
-    @Test
-    public void whenDeckIsEmpty_FunctionCountReturnsEmptyString() {
-        when(mockDeck.getCards()).thenReturn(new ArrayList<>());
+  @Test
+  public void whenDeckIsEmpty_FunctionCountReturnsEmptyString() {
+    when(mockDeck.getCards()).thenReturn(new ArrayList<>());
 
-        String expected = "";
-        String actual = testDeckBox.getFactionCardCount(mockDeck);
+    String expected = "";
+    String actual = testDeckBox.getFactionCardCount(mockDeck);
 
-        assertEquals(expected, actual);
-    }
+    assertEquals(expected, actual);
+  }
 }
