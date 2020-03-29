1d7a339d0b3f9331e9f7ef1f6dcf8ace3567c4ce
Chris <elysuim_1@hotmail.com>
2020-03-29 21:01:48 +0100

    Update PackageExe.py

--- devTools/build_exe/PackageExe.py
+++ devTools/build_exe/PackageExe.py
@@ -8,10 +8,18 @@ EXE_NAME = "CardBoard.exe"
 CLIENT_VERSION = "clientVersion.txt"
 CARD_LIST = "card_list.csv"
 
+EXAMPLE_CONFIG_LOCALHOST_NAME = "exampleLocalhostConfig.txt"
+EXAMPLE_CONFIG_PRODUCTION_SERVER_NAME =  "exampleServerConfig.txt"
+
 CARDBOARD_EXE_PATH = os.path.join(DIR_PATH, "..", "..", "Client", "target", EXE_NAME)
 CARDBOARD_BAT_PATH = os.path.join(DIR_PATH, BAT_NAME)
 CLIENTVERSION_TXT_PATH = os.path.join(DIR_PATH, "..", "..", "Client", "src", "main", "resources", CLIENT_VERSION)
 CARD_LIST_CSV_PATH = os.path.join(DIR_PATH, "..", "cardCreator", CARD_LIST)
+
+EXAMPLE_CONFIG_LOCAL_PATH = os.path.join(DIR_PATH, "..", "..", "Server", "src", "main", "resources", EXAMPLE_CONFIG_LOCALHOST_NAME)
+EXAMPLE_CONFIG_SERVER_PATH = os.path.join(DIR_PATH, "..", "..", "Server", "src", "main", "resources", EXAMPLE_CONFIG_PRODUCTION_SERVER_NAME)
+
+EXAMPLE_APPDATA_MINION_PATH = os.path.join(DIR_PATH, "..", "..", "devTools", "cardCreator", "appData_example")
 
 OUTPUT_DIR = os.path.join(DIR_PATH, "release")
 
@@ -33,10 +41,16 @@ def make_dirs(dir: str, dirname: str) -> str:
     logs_path = os.path.join(path, "logs")
     docs_path = os.path.join(path, "docs")
     data_path = os.path.join(path, "data")
+    mods_path  = os.path.join(path, "mods")
+
+    docs_configs_path = os.path.join(docs_path, "configs")
 
     os.makedirs(logs_path, exist_ok=False, mode=0x77)
     os.makedirs(docs_path, exist_ok=False, mode=0x77)
     os.makedirs(data_path, exist_ok=False, mode=0x77)
+    os.makedirs(mods_path, exist_ok=False, mode=0x77)
+
+    os.makedirs(docs_configs_path, exist_ok=False, mode=0x77)
 
     return path
 
@@ -50,6 +64,9 @@ if __name__ == "__main__":
     assert os.path.isfile(CARDBOARD_BAT_PATH), f"missing {BAT_NAME}" 
     assert os.path.isdir(OUTPUT_DIR), f"{OUTPUT_DIR} does not exist"
     assert os.path.isfile(CARD_LIST_CSV_PATH)
+    assert os.path.isfile(EXAMPLE_CONFIG_LOCAL_PATH), "missing example config"
+    assert os.path.isfile(EXAMPLE_CONFIG_SERVER_PATH), "missing example config"
+    assert os.path.isdir(EXAMPLE_APPDATA_MINION_PATH), f"{EXAMPLE_APPDATA_MINION_PATH} is not a directory."
 
     print("Getting Client version...")
     version = get_version(CLIENTVERSION_TXT_PATH)
@@ -64,6 +81,13 @@ if __name__ == "__main__":
     shutil.copy(CARDBOARD_EXE_PATH, os.path.join(release_dir, "data"))
     shutil.copy(CARDBOARD_BAT_PATH, release_dir)
     shutil.copy(CARD_LIST_CSV_PATH, os.path.join(release_dir, "docs"))
+
+    shutil.copy(EXAMPLE_CONFIG_SERVER_PATH,  os.path.join(release_dir, "docs", "configs"))
+    shutil.copy(EXAMPLE_CONFIG_LOCAL_PATH,  os.path.join(release_dir, "docs", "configs"))
+
+    # Copy ExampleMinion
+    shutil.copytree(EXAMPLE_APPDATA_MINION_PATH, os.path.join(release_dir, "mods", "card_creation"))
+
 
     print("Script COMPLETE.")
 
