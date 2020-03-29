f07ea29253fbe5fee25fffdecaacaa63ed387473
Chris <elysuim_1@hotmail.com>
2020-03-29 21:43:51 +0100

    Update ValidateCard.py

--- devTools/cardCreator/ValidateCard.py
+++ devTools/cardCreator/ValidateCard.py
@@ -1,6 +1,7 @@
+from os import name
 from typing import Dict, Any
+from distutils.util import strtobool
 
-from typeshed-fallback.stdlib.3 import json
 
 SUPPORTED_CARD_TYPES = ["HERO", "MINION", "SPELL"]
 FACTIONS = ["songhai", "vetruvian", "lyonar", "abyssian", "magmar", "vanar", "mercenary"]
@@ -11,20 +12,13 @@ def is_name_valid(filename: str, card_type: str, cardId: str) -> bool:
     
     return check_1 and check_2
 
-
-def validate_card(filename: str, card: Dict[Any, Any]) -> None:
+def validate_card(filename: str, card: Dict[Any, Any], is_custom=True) -> None:
 
     card_type = card["type"]
     assert card_type in SUPPORTED_CARD_TYPES, f"ERROR: '{card_type}' is not a supported card type."
 
     card_id   = card["cardId"]
     assert (" " not in card_id), "ERROR: Spaces should not be in cardId field"
-
-    card_name = card["name"]
-    assert card_name.replace(" ", "") == card_id, f"ERROR: 'cardId' should be the card 'name' without spaces."
-
-    card_is_custom = card.get("isCustom", False)
-    assert card_is_custom in ["True", "TRUE", "true"], "Error 'isCustom' is either missing or not set to True."
     
     card_faction = card["faction"]
     assert card_faction in FACTIONS, f"ERROR {card_faction}: is not a valid faction (case sensitive)."
@@ -34,4 +28,5 @@ def validate_card(filename: str, card: Dict[Any, Any]) -> None:
 
     assert is_name_valid(filename, card_type, card_id), f"ERROR: '{filename}' does not follow naming convention. Filename should be: {card_id}.{card_type}.card.json"
 
-
+    card_custom: str = card.get("isCustom", "False")
+    assert strtobool(card_custom) == is_custom, f"Error isCustom should be set to '{is_custom}', but was '{card_custom}''"
